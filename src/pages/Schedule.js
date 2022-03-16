f61f2695f99e26063cadb402b112cb66b02d19d0
jamestw13 <jamestw13@gmail.com>
2022-03-15 20:09:02 -0500

    loading schedule dates from google sheet

--- src/pages/Schedule.js
+++ src/pages/Schedule.js
@@ -1,68 +1,92 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import csv from 'csvtojson';
+import dayjs from 'dayjs';
 
 // 17aCoIcb2RlGMCmlN4uvUiarDWdWJa4aFqh_IZ6ZhmME
 
+function formatDate(dateString) {
+  return `${dayjs(dateString)}`;
+}
+
 function Schedule() {
-  const [scheduleData] = useState([
-    {
-      date: '4-16',
-      event: 'Easter Vigil',
-      location: 'St. Patrick Church',
-      city: 'Madison, WI',
-    },
-    {
-      date: '4-17',
-      event: 'Easter',
-      location: 'St. Patrick Church',
-      city: 'Madison, WI',
-    },
-    {
-      date: '5-27',
-      event: 'Concert',
-      location: 'Oakwood West PAC',
-      city: 'Madison, WI',
-    },
-    {
-      date: '7-20',
-      event: 'Concert in the Park',
-      location: 'Central Park',
-      city: 'Edgerton, WI',
-    },
-    {
-      date: '7-22',
-      event: 'Concert',
-      location: 'Oakwood West Courtyard',
-      city: 'Madison, WI',
-    },
-    {
-      date: '7-27',
-      event: 'Concert in the Park - Rain date',
-      location: 'Central Park',
-      city: 'Edgerton, WI',
-    },
-    {
-      date: '7-29',
-      event: 'Concert',
-      location: 'Oakwood East',
-      city: 'Madison, WI',
-    },
-  ]);
+  const [scheduleData, setScheduleData] = useState(null);
+
+  const url =
+    'https://docs.google.com/spreadsheets/d/17aCoIcb2RlGMCmlN4uvUiarDWdWJa4aFqh_IZ6ZhmME/export?format=csv';
+  const section = document.querySelector('section');
+
+  useEffect(() => {
+    fetch(url)
+      .then(result => result.text())
+      .then(csvText => csv().fromString(csvText))
+      .then(data => setScheduleData(data));
+  }, []);
+
+  //   [
+  //   {
+  //     date: '4-16',
+  //     event: 'Easter Vigil',
+  //     location: 'St. Patrick Church',
+  //     city: 'Madison, WI',
+  //   },
+  //   {
+  //     date: '4-17',
+  //     event: 'Easter',
+  //     location: 'St. Patrick Church',
+  //     city: 'Madison, WI',
+  //   },
+  //   {
+  //     date: '5-27',
+  //     event: 'Concert',
+  //     location: 'Oakwood West PAC',
+  //     city: 'Madison, WI',
+  //   },
+  //   {
+  //     date: '7-20',
+  //     event: 'Concert in the Park',
+  //     location: 'Central Park',
+  //     city: 'Edgerton, WI',
+  //   },
+  //   {
+  //     date: '7-22',
+  //     event: 'Concert',
+  //     location: 'Oakwood West Courtyard',
+  //     city: 'Madison, WI',
+  //   },
+  //   {
+  //     date: '7-27',
+  //     event: 'Concert in the Park - Rain date',
+  //     location: 'Central Park',
+  //     city: 'Edgerton, WI',
+  //   },
+  //   {
+  //     date: '7-29',
+  //     event: 'Concert',
+  //     location: 'Oakwood East',
+  //     city: 'Madison, WI',
+  //   },
+  // ]
 
   return (
     <section>
       <h2>Schedule</h2>
-      {console.log(scheduleData)}
-      {scheduleData.map((event, index) => {
-        return (
-          <div className='event-row' key={index}>
-            <p>{event.date}</p>
-            <p>{event.event}</p>
-            <p>{event.location}</p>
-            <p>{event.city}</p>
-          </div>
-        );
-      })}
+
+      {scheduleData ? (
+        scheduleData.map((show, index) => {
+          const { date, event, location } = show;
+          return (
+            <div className='event-row' key={index}>
+              <p>{formatDate(date)}</p>
+              <p>{event}</p>
+              <p>{location}</p>
+              {/* <p>{event.city}</p> */}
+            </div>
+          );
+        })
+      ) : (
+        <h1>Loading</h1>
+      )}
     </section>
   );
 }
