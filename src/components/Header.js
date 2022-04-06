c3f099c9961ea7a8958751cf2ec4661d8d3194fb
jamestw13 <jamestw13@gmail.com>
2022-04-06 11:02:28 -0500

    updated header ui

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,22 +5,31 @@ import LogoSVG from './LogoSVG';
 
 function Header() {
   return (
-    <nav className='flex bg-dark'>
+    <nav className='flex bg-dark text-accent header-bar'>
       <a href='/'>
         <LogoSVG />
       </a>
-      <div className='nav-buttons'>
-        <a href='/'>Home</a>
-
-        <a href='/about'>About</a>
-
-        <a href='/members'>Members</a>
-
-        <a href='/schedule'>Schedule</a>
-
-        <a href='/gallery'>Gallery</a>
-
-        <a href='/contact'>Contact</a>
+      <div className='nav-buttons flex'>
+        <div>
+          <a className='' href='/'>
+            Home
+          </a>
+        </div>
+        <div>
+          <a href='/about'>About</a>
+        </div>
+        <div>
+          <a href='/members'>Members</a>
+        </div>
+        <div>
+          <a href='/schedule'>Schedule</a>
+        </div>
+        <div>
+          <a href='/gallery'>Gallery</a>
+        </div>
+        <div>
+          <a href='/contact'>Contact</a>
+        </div>
       </div>
     </nav>
   );
