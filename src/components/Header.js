421573a0ead5cac60961aba45c1370b6d55a6506
TJ James <jamestw13@gmail.com>
2022-04-07 23:50:00 -0500

    added about section

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,21 +5,21 @@ import LogoSVG from './LogoSVG';
 
 function Header() {
   return (
-    <nav className='flex bg-dark text-accent header-bar'>
-      <Link to='/'>
+    <nav className="flex bg-dark text-accent header-bar">
+      <Link to="/">
         <LogoSVG />
       </Link>
-      <div className='nav-buttons flex'>
-        <Link to='/'>
+      <div className="nav-buttons flex">
+        <Link to="/">
           <p>Home</p>
         </Link>
-        {/* <Link to='/about'>
-          <p >About</p>
-        </Link> */}
+        <Link to="/about">
+          <p>About</p>
+        </Link>
         {/* <Link to='/members'>
           <p>Members</p>
         </Link> */}
-        <Link to='/schedule'>
+        <Link to="/schedule">
           <p>Schedule</p>
         </Link>
         {/* <Link to ='/gallery'>
--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -4,14 +4,20 @@ import { imageData } from '../assets/images/imageData';
 
 function Gallery() {
   return (
-    <section className='grid'>
-      <h1 className='page-header uppercase ff-sans-norm fs-600'>Gallery</h1>
+    <section className="grid">
+      <h1 className="page-header uppercase ff-sans-norm fs-600">Gallery</h1>
 
-      <section className='card gallery-box photo-grid'>
-        {imageData.map(
-          (image, index) =>
-            image.id && <Image image={image} index={image.id} key={index + 1} />
-        )}
+      <section className="card gallery-box photo-grid">
+        {imageData.map((image, index) => image.id && <Image image={image} index={image.id} key={index + 1} />)}
+        <iframe
+          className="card"
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/lUhTArdldZc?controls=0"
+          title="YouTube video player"
+          frameborder="0"
+          allowfullscreen
+        />
       </section>
     </section>
   );
