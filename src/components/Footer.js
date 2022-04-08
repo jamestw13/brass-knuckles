718714f96a1beeef3b46fdef49e4975bc2bc019f
TJ James <jamestw13@gmail.com>
2022-04-08 00:29:11 -0500

    added footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,3 +1,9 @@
 export default function Footer() {
-  return <footer className="footer-bar">Footer</footer>;
+  return (
+    <footer className="footer-bar">
+      <a href="#">Facebook</a>
+      <a href="mailto:masoncraig4@gmail.com">Email</a>
+      <a href="https://tj-james.com">&#169; TJ James</a>
+    </footer>
+  );
 }
