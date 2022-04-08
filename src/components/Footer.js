72e8823d0b2caf04e0c585f8ba654c4c879e9f27
TJ James <jamestw13@gmail.com>
2022-04-08 00:35:48 -0500

    removed unnecessary dependencies

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,10 @@
 export default function Footer() {
+  const year = new Date().getFullYear();
   return (
     <footer className="footer-bar">
       <a href="#">Facebook</a>
       <a href="mailto:masoncraig4@gmail.com">Email</a>
-      <a href="https://tj-james.com">&#169; TJ James</a>
+      <a href="https://tj-james.com">&#169; TJ James {year}</a>
     </footer>
   );
 }
