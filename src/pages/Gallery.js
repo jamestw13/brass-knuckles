ba2bddb9d41d3f7b24f5f03cef388b3ff0b76874
TJ James <jamestw13@gmail.com>
2022-04-10 10:45:32 -0500

    added gallery randomizer/limiter

--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -3,12 +3,17 @@ import Image from '../components/Image';
 import { imageData } from '../assets/images/imageData';
 
 function Gallery() {
+  const images = [];
+  for (let i = 0; i < 20; i++) {
+    const randomNumber = Math.floor(Math.random() * imageData.length);
+    images.push(imageData[randomNumber]);
+  }
   return (
     <section className="grid">
       <h1 className="page-header uppercase ff-sans-norm fs-600">Gallery</h1>
 
       <section className="card gallery-box">
-        <div className="image-container featured">
+        <div className="featured">
           <iframe
             className="video"
             src="https://www.youtube.com/embed/lUhTArdldZc?controls=0"
@@ -17,7 +22,7 @@ function Gallery() {
             allowfullscreen
           />
         </div>
-        {imageData.map((image, index) => image.id && <Image image={image} index={image.id} key={index + 1} />)}
+        {images.map((image, index) => image.id && <Image image={image} index={image.id} key={index + 1} />)}
       </section>
     </section>
   );
