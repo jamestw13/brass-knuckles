052906c8e74d293cc3cb1fb699ac1d0432845a66
TJ James <jamestw13@gmail.com>
2022-04-10 10:29:11 -0500

    added video / featured tile to gallery

--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -8,15 +8,16 @@ function Gallery() {
       <h1 className="page-header uppercase ff-sans-norm fs-600">Gallery</h1>
 
       <section className="card gallery-box">
+        <div className="image-container featured">
+          <iframe
+            className="video"
+            src="https://www.youtube.com/embed/lUhTArdldZc?controls=0"
+            title="Brass Knuckles - Three Susato Dances #1"
+            frameborder="0"
+            allowfullscreen
+          />
+        </div>
         {imageData.map((image, index) => image.id && <Image image={image} index={image.id} key={index + 1} />)}
-        <iframe
-          width="100%"
-          className="card"
-          src="https://www.youtube.com/embed/lUhTArdldZc?controls=0"
-          title="YouTube video player"
-          frameborder="0"
-          allowfullscreen
-        />
       </section>
     </section>
   );
