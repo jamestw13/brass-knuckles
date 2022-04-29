e11be441dd604a0e753f5c33bef0282c1644aa01
jamestw13 <jamestw13@gmail.com>
2022-04-28 21:01:52 -0500

    tweaked gallery ui

--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Image from '../components/Image';
 import { imageData } from '../assets/images/imageData';
 import Masonry, { ResponsiveMasonry } from 'react-responsive-masonry';
 
@@ -14,26 +13,30 @@ function Gallery() {
       <h1 className='page-header uppercase ff-sans-norm fs-600'>Gallery</h1>
 
       <section className='card'>
-        <iframe
-          className='video'
-          src='https://www.youtube.com/embed/lUhTArdldZc?controls=0'
-          title='Brass Knuckles - Three Susato Dances #1'
-          frameborder='0'
-          allowfullscreen
-        />
+        <div className='video'>
+          <iframe
+            // className='video'
+            src='https://www.youtube.com/embed/lUhTArdldZc'
+            title='Brass Knuckles - Three Susato Dances #1'
+            frameBorder='0'
+            allowFullScreen
+          />
+        </div>
 
         <ResponsiveMasonry columnsCountBreakPoints={{ 350: 1, 750: 2, 900: 3 }}>
-          <Masonry gutter={5}>
+          <Masonry>
             {imageData.map(
               (image, index) =>
                 image.id && (
-                  <img
-                    loading='lazy'
-                    className='image'
-                    src={require(`../assets/images/${index + 1}.jpg`)}
-                    description={image.description}
-                    alt=''
-                  />
+                  <div className='image' key={index}>
+                    <img
+                      loading='lazy'
+                      // className='image'
+                      src={require(`../assets/images/${index + 1}.jpg`)}
+                      description={image.description}
+                      alt=''
+                    />
+                  </div>
                 )
             )}
           </Masonry>
